096522f33d3194e6131c9897838e6ec71c008701
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-08-17 02:45:00 +0300

    commit tree 790eded2e6e5ce1397769317d6aa6e0412d0fc11

--- looping.rb
+++ looping.rb
@@ -1,6 +1,14 @@
 def happy_new_year
   # your code here
+  index = 10
+  while index <= 10 && index >=1
+    puts index
+    index -= 1  
+  end
+  puts "Happy New Year!"
 end
+happy_new_year
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -15,10 +23,27 @@ def fizzbuzz(num)
   end
 end
 
-def fizzbuzz_printer
+def fizzbuzz_printer()
   # your code here
+  num = 1
+  while num <= 100
+    puts (fizzbuzz(num))
+    num += 1  
+  end
 end
+
+fizzbuzz_printer
 
 def reverse_string(str)
   # your code here
+  reversed_string = ""
+  
+  i = str.size - 1
+  while i >= 0
+    reversed_string += str[i]
+    i-=1
+  end
+  return reversed_string
 end
+
+reverse_string("hello")
